4740e901529b33c03de9968b502e1ab32ec8c641
thatiphele <thatayaonephele@gmail.com>
2023-12-03 14:55:00 -0800

    Display String function completition

--- chars_strings.c
+++ chars_strings.c
@@ -38,3 +38,62 @@ int display_percent(va_list my_t, char my_buff[],
 	UNUSED(my_s);
 	return (write(1, "%%", 1));
 }
+/**
+ *display_string - A function that prints strings
+ *@my_buff: The buffer of array type responsible for printing
+ *@my_f: The parameter handling the active flags
+ *@my_p: The specification parameter handling the precision
+ *@my_s: The specifier parameter handling the size
+ *@my_t: The list of arguments to be printed
+ *@my_w: The parameter handling the width
+ *Return: The output of the array of strings to be displayed
+ */
+int display_string(va_list my_t, char my_buff[], int my_f,
+		int my_w, int my_p, int my_s)
+{
+	char *s = va_arg(my_t, char *);
+	int my_l = 0, x;
+
+	UNUSED(my_buff);
+	UNUSED(my_f);
+	UNUSED(my_w);
+	UNUSED(my_p);
+	UNUSED(my_s);
+
+	if (s == NULL)
+	{
+		s = "(null)";
+		if (my_p >= 6)
+			s = "      ";
+	}
+	while (s[my_l] != '\0')
+		my_l++;
+	if (my_p < my_l  && my_p >= 0)
+		my_l = my_p;
+	if (my_w * (-1) < my_l)
+	{
+		if (my_f & F_MINUS)
+		{
+			write(1, &s[0], my_l);
+			x = my_w - my_l;
+			while (x > 0)
+			{
+				write(1, " ", 1);
+				x--;
+			}
+			return (my_w);
+		}
+		else
+		{
+			x = my_w - my_l;
+			while (x > 0)
+			{
+				write(1, " ", 1);
+				x--;
+			}
+			write(1, &s[0], my_l);
+			return (my_w);
+		}
+	}
+	return (write(1, s, my_l));
+}
