c209b88fbc210214b26839f511b53986c79998fa
thatiphele <thatayaonephele@gmail.com>
2023-12-03 10:33:05 -0800

    Macro Definitions

--- main.h
+++ main.h
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+/***OTHER***/
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
+/***FLAGS***/
+#define F_SPACE 16
+#define F_HASH 8
+#define F_ZERO 4
+#define F_PLUS 2
+#define F_MINUS 1
+
+/***SIZES***/
+#define S_LONG 2
+#define S_SHORT 1
+
 
