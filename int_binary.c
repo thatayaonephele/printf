279bfb5c199358c6785048da5f9da6bcfb43c687
thatiphele <thatayaonephele@gmail.com>
2023-12-04 22:35:05 -0800

    A function that prints to the std out

--- int_binary.c
+++ int_binary.c
@@ -1,16 +1,19 @@
+#include "main.h"
 /**
  * display_int- A funciton that prints integers to the stdout
- *@my_f:
- *my_buff:
- *my_p:
- *my_s:
- *my_t:
- *my_w:
+ *@my_f:A parameter that evaluates the active flags
+ *@my_buff:The parameter that handles the array of chars
+ *@my_p:The precision specificiation parameter
+ *@my_s:The size type specification parameter to be casted
+ *@my_t:The argument list parameter
+ *@my_w:A parameter that handles the width of the array of strings
  *Return: The integer to be displayed on the stdout
  */
-int display_int(va_list my_t, char my_buff[], int my_f, int my_w, int my_p, int my_s)
+int display_int(va_list my_t, char my_buff[], int my_f,
+		int my_w, int my_p, int my_s)
 {
-	int tmp = 2, smaller_than_zero = 0, x;
+	int tmp = 2, smaller_than_zero = 0;
+	int x;
 	unsigned long my_num;
 	long lin = va_arg(my_t, long), my_const = 0;
 
@@ -33,5 +36,5 @@ int display_int(va_list my_t, char my_buff[], int my_f, int my_w, int my_p, int 
 		my_num = my_num / 10;
 	}
 	x++;
-	return (write_num(smaller_than_zero, x, my_buff, my_f, my_, my_p, my_s));
+	return (write_num(smaller_than_zero, x, my_buff, my_f, my_w, my_p, my_s));
 }
